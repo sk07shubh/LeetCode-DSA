ca97f8ef1867acb99d8c63370d5c4092098add52
Shubham Kashid <sk07shubh@gmail.com>
2026-03-22 01:32:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        if(s.length() != t.length()) {
-            return false;
+
+        unordered_map<char,int> mp;
+
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]]++;
         }
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        for(int i=0; i<t.size() ;i++){
+            char ch = t[i];
 
-        for(int i=0 ; i<s.length();i++){
-            if(s[i] != t[i]){
+            if(mp.find(ch)!=mp.end()){
+                mp[ch]--;
+                if(mp[ch]==0) mp.erase(ch);
+            }else{
                 return false;
             }
+
         }
-        return true;
-        
+        if(mp.size()==0) return true;
+
+        return false;
     }
 };
