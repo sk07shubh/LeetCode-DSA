c0e5d3a58676b16c9ae8be87e11942de42820e98
Shubham Kashid <sk07shubh@gmail.com>
2026-03-22 15:48:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -5,25 +5,15 @@ public:
 
         unordered_map<char,int> freq;
 
-        string ans = "";
-
-        for(char ch : s ){
+        for(char ch : s){
             freq[ch]++;
         }
-        
-        priority_queue<pair<int,char>> pq; 
-        for(auto [ch,Freq] : freq){
-            pq.push({Freq,ch});
 
-            
-        }
-        while(!pq.empty()){
-            auto [freq,ch] = pq.top();
-            ans += string(freq,ch);
-            pq.pop();
-        }
-        return ans;
+        sort(s.begin(),s.end(), [&] ( char a , char b){
+            return freq[a] > freq[b];
+        });
 
+        return s;
 
         
     }
