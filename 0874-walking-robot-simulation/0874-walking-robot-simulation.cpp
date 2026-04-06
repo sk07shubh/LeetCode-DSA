da0a4e0716ef0506590e1ff8f1b4a53a9e53643b
Shubham Kashid <sk07shubh@gmail.com>
2026-04-06 23:59:35 +0530

    Time: 71 ms (20.27%), Space: 42.5 MB (25.87%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        
+        unordered_set<string>st;
+        for(vector<int>obs : obstacles){
+            string key = to_string(obs[0]) + "_" + to_string(obs[1]);
+            st.insert(key);
+        }
+        int x = 0;
+        int y = 0;
+        int maxD = 0;
+
+        pair<int,int> dir = {0,1};
+
+        for(int cmd : commands){
+            if(cmd == -2) dir = {-dir.second,dir.first};
+
+            else if(cmd == -1) dir = {dir.second,-dir.first};
+
+            else{
+                for(int step=0; step < cmd; step++){
+                    int newX = x + dir.first;
+                    int newY = y + dir.second;
+
+                    string key = to_string(newX) + "_" + to_string(newY);
+                    if(st.find(key) != st.end()){
+                        break;
+                    }
+                    x = newX;
+                    y = newY;
+                }
+            }
+            maxD = max(maxD , x*x + y*y);
+
+        }
+        return maxD;
+    }
+    
+};
