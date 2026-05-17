411c75a81b5c0e71e6c7a50edb81287084de0f8b
Shubham Kashid <sk07shubh@gmail.com>
2026-05-17 22:49:14 +0530

    Time: 0 ms (100%), Space: 52.3 MB (81.51%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int maxIdx = 0;
+
+        for(int i=0; i<n; i++){
+            if(i > maxIdx) return false;
+
+            maxIdx = max(maxIdx , nums[i] + i);
+        }
+        return true;
+
+    }
+};
