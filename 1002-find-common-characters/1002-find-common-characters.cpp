9d3a550437ed4984b35039efc78b70c9c0dcd68c
Shubham Kashid <sk07shubh@gmail.com>
2026-05-30 22:59:11 +0530

    Time: 0 ms (100%), Space: 12.5 MB (57.69%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> minFreq(26, INT_MAX);
+
+        for (string& word : words) {
+            vector<int> freq(26, 0);
+
+            for (char ch : word) {
+                freq[ch - 'a']++;
+            }
+
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = min(minFreq[i], freq[i]);
+            }
+        }
+
+        vector<string> result;
+
+        for (int i = 0; i < 26; i++) {
+            while (minFreq[i]--) {
+                result.push_back(string(1, i + 'a'));
+            }
+        }
+
+        return result;
+    }
+};
