f472f8022615b44c35764382796524f42f5390d6
Shubham Kashid <sk07shubh@gmail.com>
2026-09-07 10:32:53 +0530

    Time: 4 ms (36.43%), Space: 43.2 MB (57.35%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else if(ch == ')' || ch == '}' || ch == ']'){
+                if(st.isEmpty()) return false;
+
+                if(st.peek() == '(' && ch != ')' || st.peek() == '{' && ch != '}' || st.peek() == '[' && ch != ']'){
+                    return false;
+                }
+                st.pop();
+            }
+            
+        }
+        return st.isEmpty();
+        
+    }
+}
