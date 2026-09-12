edc6ae0d520a40e3abee209c9c9bce38fd088c12
Shubham Kashid <sk07shubh@gmail.com>
2026-09-12 23:21:19 +0530

    Time: 17 ms (49.88%), Space: 43.5 MB (69.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -10,8 +10,7 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             // Odd length palindrome
             int left = i, right = i;
-            while (left >= 0 && right < s.length() &&
-                   s.charAt(left) == s.charAt(right)) {
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
                 if (right - left + 1 > maxLen) {
                     maxLen = right - left + 1;
                     start = left;
@@ -23,8 +22,7 @@ class Solution {
             // Even length palindrome
             left = i;
             right = i + 1;
-            while (left >= 0 && right < s.length() &&
-                   s.charAt(left) == s.charAt(right)) {
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
                 if (right - left + 1 > maxLen) {
                     maxLen = right - left + 1;
                     start = left;
